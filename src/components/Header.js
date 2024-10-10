7ff61b69c8138938f46ebb77874d8aaf3954b9f7
rashika-veera <rashikaveera@gmail.com>
2024-10-10 23:11:00 +0530

    hardcorded data eliminated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,8 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
+
 const Header=()=>{
+    const [btnName,setbtnName]=useState("Login")
     return (
         <>
         <div className='header-container'>
@@ -8,7 +11,9 @@ const Header=()=>{
                 <li>Home</li>
                 <li>About Us</li>
                 <li>Contact Us</li>
-
+                <button onClick={()=>{
+                btnName==="Login"?setbtnName("Logout"):setbtnName("Login")
+                }} className="Loginbtn">{btnName}</button>
             </ul>
         </div>
         </>
