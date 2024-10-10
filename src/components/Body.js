c2452d0c23efc1c8583bb71cc8fb59fc400b07ef
rashika-veera <rashikaveera@gmail.com>
2024-10-10 23:33:34 +0530

    search functionality implementation

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
-import restaurant_list from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
 import Loading from "./Loading";
 
 const Body=()=>{
     const [ResList,SetResList]=useState([]);
+    const [OriginalRestaurantList,setOriginalRestaurantList]=useState([]);
+
+    const [searchText,setsearchText]=useState("");
     useEffect(()=>{
         fetchData();
     },[]);
@@ -16,7 +18,9 @@ const Body=()=>{
         const restaurant_data=data.data.cards[4].card.card.gridElements.infoWithStyle.restaurants;
         // console.log(data.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
         SetResList(restaurant_data);
+        setOriginalRestaurantList(restaurant_data);
     }
+    
 
     if(ResList?.length===0){
         return <Loading/>
@@ -26,10 +30,18 @@ const Body=()=>{
         <>
         <div className='body-container'>
             <div className='search-container'>
-            {/* <input type='search'/> */}
+            <input type='search' value={searchText} onChange={(e)=>setsearchText(e.target.value)}/>
+            <button className="search" onClick={()=>{
+                {
+                    const filteredList=OriginalRestaurantList.filter((restaurant)=>restaurant.info.name.toLowerCase().includes(searchText.toLowerCase()) )
+                    SetResList(filteredList);
+                    console.log(filteredList)
+                }
+            }}>Search</button>
             <button className="button-top" onClick={()=>
-                {const filteredList=ResList.filter((rescards)=>rescards.info.avgRating >4 )
+                {const filteredList=OriginalRestaurantList.filter((rescards)=>rescards.info.avgRating >4.3 )
                     SetResList(filteredList)
+                    console.log(filteredList)
                 }
 
             }>Top Rated Restaurants</button>
