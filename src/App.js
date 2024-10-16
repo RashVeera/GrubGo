e397c411a1ba393dc8ea4f5165a82cc4c5a09356
rashika-veera <rashikaveera@gmail.com>
2024-10-16 18:13:22 +0530

    route management

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
-import React from 'react'
+import React, { Children } from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './components/Header';
 import Body from './components/Body'
 import Footer from './components/Footer';
+import { createBrowserRouter,RouterProvider,Outlet } from 'react-router-dom';
+import About from './components/About';
+import Error from './components/Error';
+import Contact from './components/Contact';
+import RestaurantCardMenu from './components/RestaurantCardMenu';
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
-import React from 'react'
-
 const AppLayout = () => {
   return (
     <div className='app-container'>
         <Header></Header>
-        <Body></Body>
+        <Outlet></Outlet>
         <Footer></Footer>
     </div>
   )
 }
 
-root.render(<AppLayout/>);
+const appRouter=createBrowserRouter([
+  {
+    path:"/",
+    element:<AppLayout/>,
+    errorElement:<Error/>,
+    children:[ 
+      {
+        path:"/",
+        element:<Body/>
+      },
+      {
+        path:"/about",
+        element:<About/>
+      },
+      {
+        path:"/contact",
+        element:<Contact/>
+      },
+      {
+        path:"/restaurants/:resId",
+        element:<RestaurantCardMenu/>
+      },
+    ],
+}
+])
+
+
+
+root.render(<RouterProvider router={appRouter}/>);
--- src/components/RestaurantContainer.js
+++ src/components/RestaurantContainer.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import RestaurantCard from './RestaurantCard';
+import { Link } from 'react-router-dom';
 
 const RestaurantContainer = ({ResList}) => {
     console.log(ResList,"from Restaurant Container")
   return (
     <div className='res-container'>
     { ResList.map((restaurant)=>(
-     <RestaurantCard key={restaurant.info.id} restaurantData={restaurant} />
+     <Link  key={restaurant.info.id} to={"/restaurants/"+restaurant.info.id}><RestaurantCard restaurantData={restaurant} /></Link>
      ))}
          
      </div>
