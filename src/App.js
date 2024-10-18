b6adafa6eb6a65e44df7fae93ff3a01988e9d05e
rashika-veera <rashikaveera@gmail.com>
2024-10-18 08:03:33 +0530

    inmemory routing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import Header from './components/Header';
 import Body from './components/Body'
 import Footer from './components/Footer';
-import { createBrowserRouter,RouterProvider,Outlet } from 'react-router-dom';
+import { createBrowserRouter,RouterProvider,Outlet,MemoryRouter, Routes, Route, Link  } from 'react-router-dom';
 import About from './components/About';
 import Error from './components/Error';
 import Contact from './components/Contact';
@@ -11,42 +11,59 @@ import RestaurantCardMenu from './components/RestaurantCardMenu';
 import ContactClass from './components/ContactClass';
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
+
+
 const AppLayout = () => {
+  const initialEntries = ["/", "/about", "/contact"];
   return (
+    <MemoryRouter initialEntries={initialEntries} initialIndex={0}>
     <div className='app-container'>
         <Header></Header>
-        <Outlet></Outlet>
+        <Routes>
+        <Route path="/" element={<Body />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/restaurants/:resId" element={<RestaurantCardMenu />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+
         <Footer></Footer>
     </div>
+   
+    </MemoryRouter>
   )
 }
 
-const appRouter=createBrowserRouter([
-  {
-    path:"/",
-    element:<AppLayout/>,
-    errorElement:<Error/>,
-    children:[ 
-      {
-        path:"/",
-        element:<Body/>
-      },
-      {
-        path:"/about",
-        element:<About/>
-      },
-      {
-        path:"/contact",
-        element:<ContactClass/>
-      },
-      {
-        path:"/restaurants/:resId",
-        element:<RestaurantCardMenu/>
-      },
-    ],
-}
-])
+root.render(<AppLayout />);
+
+export default AppLayout
+
+// const appRouter=createBrowserRouter([
+//   {
+//     path:"/",
+//     element:<AppLayout/>,
+//     errorElement:<Error/>,
+//     children:[ 
+//       {
+//         path:"/",
+//         element:<Body/>
+//       },
+//       {
+//         path:"/about",
+//         element:<About/>
+//       },
+//       {
+//         path:"/contact",
+//         element:<ContactClass/>
+//       },
+//       {
+//         path:"/restaurants/:resId",
+//         element:<RestaurantCardMenu/>
+//       },
+//     ],
+// }
+// ])
 
 
 
-root.render(<RouterProvider router={appRouter}/>);
+// root.render(<RouterProvider router={appRouter}/>);
