938269c2507eba6bfe1f71287cba43d60593866d
rashika-veera <rashikaveera@gmail.com>
2024-10-18 08:04:55 +0530

    optimize

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import Header from './components/Header';
 import Body from './components/Body'
 import Footer from './components/Footer';
-import { createBrowserRouter,RouterProvider,Outlet,MemoryRouter, Routes, Route, Link  } from 'react-router-dom';
+import { createBrowserRouter,RouterProvider,Outlet  } from 'react-router-dom';
 import About from './components/About';
 import Error from './components/Error';
 import Contact from './components/Contact';
@@ -14,56 +14,42 @@ const root=ReactDOM.createRoot(document.getElementById("root"));
 
 
 const AppLayout = () => {
-  const initialEntries = ["/", "/about", "/contact"];
   return (
-    <MemoryRouter initialEntries={initialEntries} initialIndex={0}>
     <div className='app-container'>
         <Header></Header>
-        <Routes>
-        <Route path="/" element={<Body />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/restaurants/:resId" element={<RestaurantCardMenu />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-
+        <Outlet></Outlet>
         <Footer></Footer>
     </div>
-   
-    </MemoryRouter>
   )
 }
 
-root.render(<AppLayout />);
 
-export default AppLayout
-
-// const appRouter=createBrowserRouter([
-//   {
-//     path:"/",
-//     element:<AppLayout/>,
-//     errorElement:<Error/>,
-//     children:[ 
-//       {
-//         path:"/",
-//         element:<Body/>
-//       },
-//       {
-//         path:"/about",
-//         element:<About/>
-//       },
-//       {
-//         path:"/contact",
-//         element:<ContactClass/>
-//       },
-//       {
-//         path:"/restaurants/:resId",
-//         element:<RestaurantCardMenu/>
-//       },
-//     ],
-// }
-// ])
+const appRouter=createBrowserRouter([
+  {
+    path:"/",
+    element:<AppLayout/>,
+    errorElement:<Error/>,
+    children:[ 
+      {
+        path:"/",
+        element:<Body/>
+      },
+      {
+        path:"/about",
+        element:<About/>
+      },
+      {
+        path:"/contact",
+        element:<ContactClass/>
+      },
+      {
+        path:"/restaurants/:resId",
+        element:<RestaurantCardMenu/>
+      },
+    ],
+}
+])
 
 
 
-// root.render(<RouterProvider router={appRouter}/>);
+root.render(<RouterProvider router={appRouter}/>);
