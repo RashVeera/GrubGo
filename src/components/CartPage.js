654f77ad61486d54c27da0f1d13dd2e4df972268
rashika-veera <rashikaveera@gmail.com>
2024-10-25 10:32:04 +0530

    test func

--- src/components/CartPage.js
+++ src/components/CartPage.js
@@ -36,7 +36,7 @@ const CartPage = () => {
     <span>To Pay</span>
     <span>Rs. {price}</span>
     </div>
-    <Link to="/payment"><button className='ml-48 border border-solid border-orange-800 bg-orange-500  rounded-sm px-4 py-2 font-bold  text-white'>Proceed to Payment</button></Link>
+    <Link to="/payment"><button onClick={cleartheCart} className='ml-48 border border-solid border-orange-800 bg-orange-500  rounded-sm px-4 py-2 font-bold  text-white'>Proceed to Payment</button></Link>
     </>
     ):
     (
